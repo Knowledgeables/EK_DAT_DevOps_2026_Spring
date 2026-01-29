6f1e3c6b94ca818f79ee43bbd48c1f784d16ff7a
Kristian Meyer Steffensen <krst0003@stud.ek.dk>
2026-01-29 15:48:49 +0100

    Added ripmarkus to repositories.py

--- repositories.py
+++ repositories.py
@@ -8,5 +8,16 @@ GROUP_REPOS = [
     "stack": [],
     "documentation": [],
     "sla": "",
+},
+{
+    "name": "ripmarkus",
+    "gitLinks": ["https://github.com/Gorillaerne/DevOps26"],
+    "backend": "",
+    "frontend": "",
+    "monitoring": "",
+    "stack": [],
+    "documentation": [],
+    "sla": "",
 }
+
 ]
